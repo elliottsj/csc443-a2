e498db8a30a8e27784a01971104610c079cdbe94
Alec Brunelle <alec@alec.coffee>
2016-10-28 15:13:37 -0400

    Add serialization methods

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # csc443-a2
+
+### 2. Record serialization
+
+Record is an array of strings: `['Bob', 'Sagot', '51 Bahen Ave']`
--- library.cc
+++ library.cc
@@ -1,7 +1,36 @@
-#include <iostream>
+#include <numeric>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+typedef const char* V;
+typedef std::vector<V> Record;
+
+
+/**
+  * Returns result plus length of current.
+  */
+int sum_strlen(int result, char* current){
+    // Apparently, strlen(current) will always be the same size
+    // https://goo.gl/KRneIj
+    return result + strlen(current);
 }
+
+/**
+ * Compute the number of bytes required to serialize record.
+ */
+int fixed_len_sizeof(Record *record){
+    return std::accumulate (record.begin(), record.end(), 0, sum_strlen);
+};
+
+/**
+ * Serialize the record to a byte array to be stored in buf.
+ */
+void fixed_len_write(Record *record, void *buf){
+    // iterate through the record
+    for (int j = 0; j < record.size(); j++){
+        // copy the string into the buf
+        int string_length = strlen(record[j]);
+        int position = (j * string_length);
+        memcpy((char*) buf + position, record[j], string_length);
+    }
+};
