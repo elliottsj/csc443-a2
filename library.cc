29969be7901722818e6eacf308b53573eb2c17e0
Spencer Elliott <me@elliottsj.com>
2016-10-28 16:31:35 -0400

    Fix compile errors

--- library.cc
+++ library.cc
@@ -9,29 +9,29 @@ typedef std::vector<V> Record;
 /**
   * Returns result plus length of current.
   */
-int sum_strlen(int result, char* current){
+int sum_strlen(int result, const char* current) {
     // Apparently, strlen(current) will always be the same size
     // https://goo.gl/KRneIj
-    return result + strlen(current);
+    return result + std::strlen(current);
 }
 
 /**
  * Compute the number of bytes required to serialize record.
  */
-int fixed_len_sizeof(Record *record){
-    return std::accumulate (record.begin(), record.end(), 0, sum_strlen);
+int fixed_len_sizeof(Record *record) {
+    return std::accumulate(record->begin(), record->end(), 0, sum_strlen);
 };
 
 /**
  * Serialize the record to a byte array to be stored in buf.
  */
-void fixed_len_write(Record *record, void *buf){
+void fixed_len_write(Record *record, void *buf) {
     // iterate through the record
-    for (int j = 0; j < record.size(); j++){
+    for (int j = 0; j < record->size(); j++){
         // copy the string into the buf
-        int string_length = strlen(record[j]);
+        int string_length = std::strlen(record->at(j));
         int position = (j * string_length);
-        std::memcpy((char*) buf + position, record[j], string_length);
+        std::memcpy((char*) buf + position, record->at(j), string_length);
     }
 };
 
@@ -40,8 +40,8 @@ void fixed_len_write(Record *record, void *buf){
  * stores the record in `record`.
  */
 void fixed_len_read(void *buf, int size, Record *record) {
-    int value_length = size / record.size();
-    for (int i = 0; i < record.size(); i++) {
-        memcpy(record[i], ((char *) buf) + i * value_length);
+    int value_length = size / record->size();
+    for (int i = 0; i < record->size(); i++) {
+        std::memcpy((void *) record->at(i), ((char *) buf) + i * value_length, value_length);
     }
 }
