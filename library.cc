4bf3dd8f7aadd63f4c08500b98ac6c63ea40fe53
Alec Brunelle <alec@alec.coffee>
2016-11-04 19:53:41 -0400

    Add page functions to library

--- library.cc
+++ library.cc
@@ -45,3 +45,38 @@ void fixed_len_read(void *buf, int size, Record *record) {
         std::memcpy((void *) record->at(i), ((char *) buf) + i * value_length, value_length);
     }
 }
+
+/**
+ * Initializes a page using the given slot size
+ */
+void init_fixed_len_page(Page *page, int page_size, int slot_size){
+
+};
+
+/**
+ * Calculates the maximal number of records that fit in a page
+ */
+int fixed_len_page_capacity(Page *page);
+
+/**
+ * Calculate the free space (number of free slots) in the page
+ */
+int fixed_len_page_freeslots(Page *page);
+
+/**
+ * Add a record to the page
+ * Returns:
+ *   record slot offset if successful,
+ *   -1 if unsuccessful (page full)
+ */
+int add_fixed_len_page(Page *page, Record *r);
+
+/**
+ * Write a record into a given slot.
+ */
+void write_fixed_len_page(Page *page, int slot, Record *r);
+
+/**
+ * Read a record from the page from a given slot.
+ */
+void read_fixed_len_page(Page *page, int slot, Record *r);
