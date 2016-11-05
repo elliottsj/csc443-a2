0d4cee116a5506586e61300fef2b9f49d14cf14b
Alec Brunelle <alec@alec.coffee>
2016-11-04 23:26:55 -0400

    Add fixed_len_page_freeslots, write_fixed_len_page, add_fixed_len_page, read_fixed_len_page
    Add little changes to adhere to gcc
    
    Alter the way directory_size is initialized
    
    Spencers comments!
    
    Moar of spencer

--- library.cc
+++ library.cc
@@ -6,6 +6,13 @@
 typedef const char* V;
 typedef std::vector<V> Record;
 
+typedef struct {
+    void *data; // Points to a byte array (Record).
+    int page_size; // The total page_size, implicitly includes size_of_directory
+    int slot_size; // How big a Record is.
+    int size_of_directory; // The size of our directory in bytes.
+} Page;
+
 
 /**
   * Returns result plus length of current.
@@ -48,80 +55,125 @@ void fixed_len_read(void *buf, int size, Record *record) {
 }
 
 
-typedef struct {
-    void *data; // Points to a byte array (Record).
-    int page_size;
-    int slot_size; // How big a Record is.
-    int directory_offset; // How many slots away from the beginning is our initial Record.
-} Page;
-
-
 /**
  * Calculates the maximal number of records that fit in a page
  */
 int fixed_len_page_capacity(Page *page){
     int num_slots = page->page_size/page->slot_size;
 
-    // in bytes
-    int size_of_directory = num_slots/8;
-
-    // find out how much slots our directory is going to need, round up
-    int number_of_directory_slots = ceil(size_of_directory/8 / page->slot_size);
+    // find out how many slots our directory is going to need
+    int number_of_directory_slots = std::ceil(page->size_of_directory / page->slot_size);
 
     return num_slots - number_of_directory_slots;
 };
 
+
+/** Initialize the empty directory.
+
+ *  A directory which holds a 0 if a slot is empty or a 1 if the slot has data.
+ */
+void init_directory(Page *page){
+    for (int i = 0; i < page->size_of_directory;i++) {
+        char* b = ((char *) page->data) + page->page_size - page->size_of_directory + i;
+        *b = 0;
+    }
+}
+
 /**
  * Initializes a page using the given slot size.
- * Calculates size of directory and initilizes inside the data. A directory is held
+ * Calculates size of directory and puts it in inside the page data. A directory is held
  * at the end of the data in the Page.
+
+ * A directory is a bitmap.
+ * Directory contents are traversed backwards.
+
+ * Say we want to insert a record, we look into the directory to figure out
+ * which slot is free. Traversing backwards inside the directory allows us to
+ * not keep track of directory offset.
 
  * Accepts only clean divisions of page_size/slot_size.
  */
 void init_fixed_len_page(Page *page, int page_size, int slot_size){
     int num_slots = page_size/slot_size;
 
-    // in bytes
-    int size_of_directory = num_slots/8;
-
     page->page_size = page_size;
     page->slot_size = slot_size;
-
-    int total_page_slots = fixed_len_page_capacity(page);
+    // size_of_directory is in bytes
+    page->size_of_directory = std::ceil(num_slots/8);
 
     void *new_data = malloc(page_size);
     page->data = new_data;
 
-    // insert directory at the end of the page.
-
-    // Initialize the empty directory.
-    // A directory which holds a 0 if a slot is empty or a 1 if the slot has data.
-    for (int i = 0; i < size_of_directory;i++) {
-        char* b = ((char *) page->data) + page_size - size_of_directory + i;
-        *b = 0;
-    }
+    // Insert directory at the end of the page.
+    init_directory(page);
 };
 
 
 /**
- * Calculate the free space (number of free slots) in the page
- */
-int fixed_len_page_freeslots(Page *page);
+ * Calculate the free space (number of free slots) in the page.data
 
-/**
- * Add a record to the page
  * Returns:
- *   record slot offset if successful,
- *   -1 if unsuccessful (page full)
+ *  Number of free slots.
  */
-int add_fixed_len_page(Page *page, Record *r);
+int fixed_len_page_freeslots(Page *page){
+    int freeslots = 0;
+    // get the directory
+    unsigned char* directory = ((unsigned char *) page->data) + page->page_size - page->size_of_directory;
+    // traverse the directory and calculate the number of free slots
+    for (int i = 0; i < page->size_of_directory;i++){
+        for (int j = 0;j < 8;j++) {
+            char shifted = directory[i] >> j;
+            freeslots += (shifted & 1) == 0;
+        }
+    }
+    return freeslots;
+};
 
 /**
  * Write a record into a given slot.
  */
-void write_fixed_len_page(Page *page, int slot, Record *r);
+void write_fixed_len_page(Page *page, int slot, Record *r){
+    // get the slot
+    unsigned char* slot_in_page = ((unsigned char *) page->data) + page->slot_size * slot;
+    // serialize the data in r and write to slot_in_page
+    fixed_len_write(r, (void *) slot_in_page);
+}
+
+/**
+ * Add a record to the page.
+ * Returns:
+ *   record slot offset if successful,
+ *   -1 if unsuccessful (page full)
+ */
+int add_fixed_len_page(Page *page, Record *r){
+    // get free slot
+    unsigned char* directory = ((unsigned char *) page->data) + page->page_size;
+    int first_free_slot = -1;
+    for (int i = page->size_of_directory - 1; i >= 0;i--){
+        for (int j = 7; j >= 0; j--){
+            char shifted = directory[i] >> j;
+            if ((shifted & 1) == 0){
+                first_free_slot = j;
+                break;
+            }
+        }
+        if (first_free_slot != -1){
+            break;
+        }
+    }
+
+    // write the record to the slot
+    write_fixed_len_page(page, first_free_slot, r);
+
+    return first_free_slot;
+}
 
 /**
  * Read a record from the page from a given slot.
  */
-void read_fixed_len_page(Page *page, int slot, Record *r);
+void read_fixed_len_page(Page *page, int slot, Record *r){
+    // get the data slot in the page
+    char* data_slot = ((char *) page->data) + page->slot_size * slot;
+    // serialize the data at the dataslot and store in r
+    fixed_len_read(data_slot, page->slot_size, r);
+}
