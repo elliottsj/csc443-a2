507151fc1d14935772266945512e6ba2266b5a4b
Spencer Elliott <me@elliottsj.com>
2016-11-08 16:00:54 -0500

    Convert mkcsv.py to python 3

--- mkcsv.py
+++ mkcsv.py
@@ -6,14 +6,14 @@ try:
     filename, n_tuples = sys.argv[1:3]
     n = int(n_tuples)
 except:
-    print 'Usage: %s <outfile> <number of tuples>' % sys.argv[0]
+    print('Usage: {} <outfile> <number of tuples>'.format(sys.argv[0]))
     sys.exit(0)
 
 with open(filename, 'w') as f:
-    for _ in xrange(n):
+    for _ in range(n):
         row = []
         for a in range(100):
             row.append(''.join([random.choice(letters) for j in range(10)]))
-        print >>f, ",".join(row)
+        f.write(",".join(row) + "\n")
 
-print "Generated %d random tuples in %s." % (n, filename)        
+print("Generated {} random tuples in {}.".format(n, filename))
