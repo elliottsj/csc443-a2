9efaf43596434cf0bd84524881df53227db7bb1b
Alec Brunelle <alec@alec.coffee>
2016-11-15 11:42:16 -0500

    experiments, on to other things

--- experiment_write_fixed_len_page.py
+++ experiment_write_fixed_len_page.py
@@ -17,7 +17,7 @@ def write_fixed_len_pages(csv_file, output_file, page_size):
             os.path.join(os.path.dirname(os.path.realpath(__file__)), './write_fixed_len_pages'),
             csv_file,
             output_file,
-            str(block_size),
+            str(page_size),
         ],
         stdout=subprocess.PIPE,
     )
@@ -35,36 +35,34 @@ def main():
 
     """
     if len(sys.argv) < 4:
-        print('Usage: experiment_get_histogram <csv_file> <output_file>, <page_size>')
+        print('Usage: experiment_get_histogram <csv_file> <output_file>')
         sys.exit(1)
     csv_file = sys.argv[1]
     output_file = sys.argv[2]
-    page_size = sys.argv[3]
 
-    block_sizes = [
+    page_sizes = [
         128,           # 128 B
         512,           # 512 B
         1 * 2 ** 10,   # 1 KiB
         4 * 2 ** 10,   # 4 KiB
         8 * 2 ** 10,   # 8 KiB
-        64 * 2 ** 10,  # 64 KiB
-        128 * 2 ** 10,  # 128 KiB
-        512 * 2 ** 10,  # 512 KiB
-        1 * 2 ** 20,   # 1 MiB
-        2 * 2 ** 20,   # 2 MiB
+        # 64 * 2 ** 10,  # 64 KiB
+        # 128 * 2 ** 10,  # 128 KiB
+        # 512 * 2 ** 10,  # 512 KiB
+        # 1 * 2 ** 20,   # 1 MiB
+        # 2 * 2 ** 20,   # 2 MiB
     ]
-    total_size = 100 * 2 ** 20  # 100 MiB
 
     if os.path.exists('./out'):
         shutil.rmtree('./out')
     os.mkdir('./out')
     csvwriter = csv.DictWriter(
         sys.stdout,
-        fieldnames=('block_size', 'milliseconds_elapsed'),
+        fieldnames=('page_size', 'milliseconds_elapsed'),
         dialect='unix'
     )
     csvwriter.writeheader()
-    for block_size in block_sizes:
+    for page_size in page_sizes:
         for i in range(50):
             ms_elapsed = write_fixed_len_pages(
                 csv_file,
@@ -72,7 +70,7 @@ def main():
                 page_size,
             )
             csvwriter.writerow({
-                'block_size': block_size,
+                'page_size': page_size,
                 'milliseconds_elapsed': ms_elapsed,
             })
 
